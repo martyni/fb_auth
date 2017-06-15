2bee4c80295cf9a48ecc0a2afd63c43de452aba4
martyni <martynjamespratt@gmail.com>
2017-06-15 13:30:09 +0000

    added pictures folder

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,7 +2,7 @@ source env/bin/activate
 zappa update $1 | tee build.log
 awk -F "live!:" /amazonaws.com/'{print $2}' build.log > url
 
-for file in $(ls app/static/)
+for file in $(ls app/static/) $(ls app/static/*/*)
 	do s3_static martyni-static auth $1 app/static/$file
 done	
 
