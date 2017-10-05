813970509b7cf1b963bf773424b0dbd8ed8a3918
martyni <martynjamespratt@gmail.com>
2017-10-05 11:16:32 +0000

    updated

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,6 +1,6 @@
 cp ~/secrets.py app/
 virtualenv env
 source env/bin/activate
-pip install -r requirements.txt
+pip install feedgen
 pip install -r requirements.txt --upgrade
 pip install . --upgrade
